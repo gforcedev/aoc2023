f592ad65f693e3255804aa5907e17dd03da37906
Gforcedev <tecshotclark@gmail.com>
2023-12-01 20:54:44 +0000

    Copy last year's scaffolding

---
+++ README.md
@@ -0,0 +1,4 @@
+# aoc2023
+Again
+
+Hopefully the rest aren't as cursed as day 1 but it's the end of a tough week...
---
+++ TEMPLATE.ts
@@ -0,0 +1,7 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync("src/TODAY/input.txt").toString();
+
+const answer = input;
+
+console.log(answer);
