f6c91494262b765ddd773b3da96e712cdfe642c0
Gforcedev <tecshotclark@gmail.com>
2023-12-05 21:16:04 +0000

    Add day 5, part B could be optimised but I got the right answer just more slowly

---
+++ src/5/a.ts
@@ -0,0 +1,50 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync("src/5/input.txt").toString();
+
+type Mapping = {
+  destStart: number;
+  sourceStart: number;
+  length: number;
+};
+
+const seeds = input
+  .split("\n")[0]
+  .match(/\d+/g)!
+  .map((n) => parseInt(n, 10));
+
+const parsed = input
+  .slice(2, -1)
+  .split("\n\n")
+  .map((block) => block.split("\n").slice(1))
+  .map((mappings) =>
+    mappings.map((m) => {
+      const matches = m.match(/\d+/g);
+      return {
+        destStart: parseInt(matches![0]!, 10),
+        sourceStart: parseInt(matches![1], 10),
+        length: parseInt(matches![2], 10),
+      } as Mapping;
+    })
+  );
+
+let lowestLocation = Infinity;
+for (let seed of seeds) {
+  let currNum = seed;
+  for (let mappingSet of parsed) {
+    for (let mapping of mappingSet) {
+      if (
+        currNum >= mapping.sourceStart &&
+        currNum < mapping.sourceStart + mapping.length
+      ) {
+        currNum = mapping.destStart + (currNum - mapping.sourceStart);
+        break;
+      }
+    }
+  }
+  if (currNum < lowestLocation) {
+    lowestLocation = currNum;
+  }
+}
+
+console.log(lowestLocation);
