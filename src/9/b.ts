fe62db7a9236f7b90f83e2b4fea8f1c7512915aa
Gforcedev <tecshotclark@gmail.com>
2023-12-09 11:06:00 +0000

    Add day 9

---
+++ src/9/b.ts
@@ -0,0 +1,46 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync("src/9/input.txt")
+  .toString()
+  .split("\n")
+  .slice(0, -1);
+
+const parsed = input.map((line) =>
+  line
+    .match(/[-\d]+/g)!
+    .map((s) => parseInt(s, 10))
+    .reverse()
+);
+
+function diffs(sequences: number[][]): number[][] {
+  const thisSequence = sequences[sequences.length - 1];
+  const thisDiffList = [];
+  for (let i = 0; i < thisSequence.length - 1; i++) {
+    thisDiffList.push(thisSequence[i + 1] - thisSequence[i]);
+  }
+
+  if (new Set(thisSequence).size === 1) {
+    return sequences;
+  }
+
+  return diffs([...sequences, thisDiffList]);
+}
+
+function nextFromDiffs(sequences: number[][]) {
+  for (let i = sequences.length - 1; i > 0; i--) {
+    sequences[i - 1].push(
+      sequences[i - 1][sequences[i - 1].length - 1] +
+        sequences[i][sequences[i].length - 1]
+    );
+  }
+
+  return sequences[0].pop()!;
+}
+
+const answer = parsed
+  .map((sequence) => [sequence])
+  .map(diffs)
+  .map(nextFromDiffs)
+  .reduce((a, b) => a + b);
+
+console.log(answer);
